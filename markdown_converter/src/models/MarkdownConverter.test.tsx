6d4f46b5cd5968f736b64319ddcaad68763f5a5d
Krisztian Kis <christian.kis.dev@gmail.com>
2020-09-29 13:01:58 -0700

    Added rowChunker to chunk multirow input to lines, and to combine multiline paragraphs. (~3 hours in)

--- markdown_converter/src/models/MarkdownConverter.test.tsx
+++ markdown_converter/src/models/MarkdownConverter.test.tsx
@@ -1,4 +1,4 @@
-import { MarkdownConverter } from './MarkdownConverter';
+import { Empty, Header, MarkdownConverter, Paragraph } from './MarkdownConverter';
 
 describe('MarkdownConverter', () => {
     let converter: MarkdownConverter;
@@ -76,5 +76,85 @@ describe('MarkdownConverter', () => {
         const result = converter.convert(input);
 
         expect(result).toEqual("<h3>Some header</h3>")
+    });
+
+    describe('chunkText', () => {
+        let converter: MarkdownConverter;
+
+        beforeEach(() => {
+            converter = new MarkdownConverter();
+        });
+
+        it('throws exception when input text is null', () => {
+            expect(() => converter.chunkText(null)).toThrow(new Error("Input is null or undefined."));
+        });
+
+        it('throws exception when input text is undefined', () => {
+            expect(() => converter.chunkText(undefined)).toThrow(new Error("Input is null or undefined."));
+        });
+
+        it('returns empty string for empty input', () => {
+            const result = converter.chunkText("");
+
+            expect(result[0] instanceof Empty).toBe(true);
+            expect(result[0].rawText).toEqual("");
+        });
+
+        it('returns a paragraph for a single line of unformatted text', () => {
+            const result = converter.chunkText("Some text");
+
+            expect(result[0] instanceof Paragraph).toBe(true);
+            expect(result[0].rawText).toEqual("Some text");
+        });
+
+        it('returns a header for a single line of header text', () => {
+            const result = converter.chunkText("# Some text");
+
+            expect(result[0] instanceof Header).toBe(true);
+            expect(result[0].rawText).toEqual("# Some text");
+        });
+
+        it('returns two headers for two lines of headers', () => {
+            const result = converter.chunkText("# Some text\n# Some other text");
+
+            expect(result.length).toBe(2);
+            expect(result[0] instanceof Header).toBe(true);
+            expect(result[0].rawText).toEqual("# Some text");
+            expect(result[1] instanceof Header).toBe(true);
+            expect(result[1].rawText).toEqual("# Some other text");
+        });
+
+        it('returns a single paragraph for two lines of unformatted text', () => {
+            const result = converter.chunkText("Some text\nSome other text");
+
+            expect(result.length).toBe(1);
+            expect(result[0] instanceof Paragraph).toBe(true);
+            expect(result[0].rawText).toEqual("Some text\nSome other text");
+        });
+
+        it('returns paragraph, header, paragraph when there is a header between two paragraphs', () => {
+            const result = converter.chunkText("Some text\n# Header\nSome other text");
+
+            expect(result.length).toBe(3);
+            expect(result[0] instanceof Paragraph).toBe(true);
+            expect(result[0].rawText).toEqual("Some text");
+            expect(result[1] instanceof Header).toBe(true);
+            expect(result[1].rawText).toEqual("# Header");
+            expect(result[2] instanceof Paragraph).toBe(true);
+            expect(result[2].rawText).toEqual("Some other text");
+        });
+
+        it('returns two paragraphs when there is an extra empty row between two paragraphs', () => {
+            const result = converter.chunkText("Some text\n\nSome other text");
+
+            console.log(result)
+
+            expect(result.length).toBe(3);
+            expect(result[0] instanceof Paragraph).toBe(true);
+            expect(result[0].rawText).toEqual("Some text");
+            expect(result[1] instanceof Empty).toBe(true);
+            expect(result[2] instanceof Paragraph).toBe(true);
+            expect(result[2].rawText).toEqual("Some other text");
+        });
     });
 });
--- markdown_converter/src/models/MarkdownConverter.tsx
+++ markdown_converter/src/models/MarkdownConverter.tsx
@@ -1,39 +1,118 @@
 export class MarkdownConverter {
 
     public convert = (inputText: string): string => {
-        if(inputText == null) {
+        if (inputText == null) {
             throw new Error("Input is null or undefined.");
         }
-        
-        if(inputText === "") {
+
+        if (inputText === "") {
             return inputText;
         }
 
-        if(inputText[0] === '#') {
+        if (inputText[0] === '#') {
             let headerCounter = 1;
             let currentCharacterIndex = 1;
-            while(currentCharacterIndex < inputText.length && headerCounter < 6 && inputText[currentCharacterIndex] === '#') {
+            while (currentCharacterIndex < inputText.length && headerCounter < 6 && inputText[currentCharacterIndex] === '#') {
                 headerCounter++;
                 currentCharacterIndex++;
             }
 
             const textAfterHashtags = inputText.substring(headerCounter);
 
-            if(!textAfterHashtags.trim()) {
+            if (!textAfterHashtags.trim()) {
                 return "";
             }
 
-            if(textAfterHashtags[0] === ' ') {
+            if (textAfterHashtags[0] === ' ') {
                 const trimmedText = textAfterHashtags.trim();
 
                 return `<h${headerCounter}>${trimmedText}</h${headerCounter}>`;
             }
         }
-        
-        if(inputText.startsWith("# ")) {
+
+        if (inputText.startsWith("# ")) {
             return `<h1>${inputText.substring(2)}</h1>`;
         }
 
         return `<p>${inputText}</p>`
+    }
+
+    public chunkText = (text: string): Element[] => {
+        if (text == null) {
+            throw new Error("Input is null or undefined.");
+        }
+
+        let elements: Element[] = [];
+
+        const rows = text.split('\n');
+        let currentMultilineParagraphs: Paragraph[] = [];
+
+        for(let rowCount = 0; rowCount < rows.length; rowCount++) {
+            const element = this.decideRow(rows[rowCount]);
+            if(element instanceof Paragraph) {
+                currentMultilineParagraphs.push(element);
+            } else {
+                if(currentMultilineParagraphs.length > 0) {
+                    const paragraph = this.combineParagraphs(currentMultilineParagraphs);
+                    elements.push(paragraph);
+                    currentMultilineParagraphs = [];
+                }
+
+                elements.push(element);
+            }
+        }
+
+        if(currentMultilineParagraphs.length > 0) {
+            const paragraph = this.combineParagraphs(currentMultilineParagraphs);
+            elements.push(paragraph);
+        }
+
+        return elements
+    };
+
+    private combineParagraphs = (paragraphs: Paragraph[]): Paragraph => {
+        let texts: string[] = [];
+
+        paragraphs.forEach(paragraph => texts.push(paragraph.rawText));
+
+        let combinedText = texts.join("\n");
+
+        return new Paragraph(combinedText);
+    };
+
+    private decideRow = (text: string): Element => {
+        if (text === "") {
+            return new Empty();
+        }
+
+        if (RegExp(/^#{1,6}\s\S/).test(text)) {
+            return new Header(text);
+        }
+
+        return new Paragraph(text);
+    };
+}
+
+export interface Element {
+    rawText: string;
+}
+
+export class Paragraph implements Element {
+    rawText: string;
+
+    constructor(text: string) {
+        this.rawText = text;
+    }
+}
+
+export class Empty implements Element {
+    rawText = "";
+}
+
+export class Header implements Element {
+    rawText: string;
+
+    constructor(text: string) {
+        this.rawText = text;
     }
 }
